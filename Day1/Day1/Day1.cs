af74b4723cf57178594077c5088740bc49b894c4
CKYong <ckyoung@sogyo.nl>
2017-06-14 14:39:31 +0200

    Finished Day1Part1

---
+++ Day1/Day1/Day1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] walkerInput = Resources.Resources.Day1Input.Split(new string[] { ", " }, StringSplitOptions.None);
+            Walker walker = new Walker();
+            walker.walk(walkerInput);
+            Console.WriteLine("Walker finished traveling, distance of endpoint:\n" + walker.getDistance());
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ Day1/Day1Test/Day1Test.cs
@@ -0,0 +1,31 @@
+﻿using Day1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day1Tests
+{
+    [TestClass]
+    public class Day1Test
+    {
+        private Walker walker = new Walker();
+
+        [TestInitialize]
+        public void reset()
+        {
+            walker.reset();
+        }
+
+        [TestMethod]
+        public void parseR2L3ShouldGiveDistanceOf5()
+        {
+            walker.walk(new string[] { "R2", "L3" });
+            Assert.AreEqual(5, walker.getDistance());
+        }
+
+        [TestMethod]
+        public void turnLeftWalk100ShouldReturn100()
+        {
+            walker.walk(new string[] { "L100" });
+            Assert.AreEqual(100, walker.getDistance());
+        }
+    }
+}
